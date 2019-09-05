7881b9a1845d82817c631827405f2a75717c8fe4
Sowmya1996 <sowmyapraksh1967@gmail.com>
2019-09-05 20:45:02 +0530

    Added student index action

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root 'courses#index'
   get 'courses/new', to: 'courses#new'
   get 'about', to: 'pages#about'
+  resources :students
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
