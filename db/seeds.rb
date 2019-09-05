e5c9d5d2a7c0675b50567718dfde1ac525f97c44
Sowmya1996 <sowmyapraksh1967@gmail.com>
2019-09-05 19:42:02 +0530

    Added validation and fetching information from db through view

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Course.create(short_name: "CS205", 
+name: "Intro to Linear Algebra", 
+description: "An essential introduction to Linear Algebra and how it impacts the modern world of machine learning")
+course = Course.find(2)
+course.description= "An introductory look at data structure and establishing an ubderstanding of basic algorithms"
